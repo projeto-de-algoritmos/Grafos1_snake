6e41ad0575a8d72d4c333d60800b4d019a91599b
Dara Maria <darasousa09@gmail.com>
2023-07-21 21:31:51 -0300

    Adicionando niveis do jogo

--- game.py
+++ game.py
@@ -3,18 +3,47 @@ import time
 import random
 from snake import Snake
 
+class Level:
+    def __init__(self, name, snake_speed):
+        self.name = name
+        self.snake_speed = snake_speed
+
 class Game:
     def __init__(self):
-        # Set up the screen and other game variables
         self.screen_width = 800
         self.screen_height = 600
         self.game_display = pygame.display.set_mode((self.screen_width, self.screen_height))
         pygame.display.set_caption('Snake Game')
         self.clock = pygame.time.Clock()
         self.block_size = 20
-        self.snake_speed = 15
+        
+        self.levels = [
+            Level("Fácil", 10),
+            Level("Médio", 15),
+            Level("Difícil", 20)
+        ]
+        self.current_level = 0
+
+        self.snake_speed = 5
         self.snake = Snake(self.screen_width, self.screen_height, self.block_size)
         self.food = self.generate_food()
+
+    def select_level(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_1:
+                        self.current_level = 0  
+                        return
+                    elif event.key == pygame.K_2:
+                        self.current_level = 1  
+                        return
+                    elif event.key == pygame.K_3:
+                        self.current_level = 2  
+                        return
     
     def generate_food(self):
         food_x = round(random.randrange(0, self.screen_width - self.block_size) / self.block_size) * self.block_size
@@ -42,11 +71,12 @@ class Game:
                 self.snake.grow()
                 self.food = self.generate_food()
 
+
             self.game_display.fill((255, 255, 255))
             self.draw_food()
             self.snake.draw(self.game_display)
 
             pygame.display.update()
-            self.clock.tick(self.snake_speed)
-
+            self.clock.tick(self.levels[self.current_level].snake_speed)
+        
         pygame.quit()
